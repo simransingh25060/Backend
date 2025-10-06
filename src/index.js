8811c3a51ed1b5e4a80fc1e2643110ce7446fb61
Simran Singh <simransingh25060@gmail.com>
2025-10-06 23:58:48 +0530

    connect to mongoDB

---
+++ src/index.js
@@ -0,0 +1,44 @@
+
+import dotenv from "dotenv";
+import connectDB from "./db";
+
+
+dotenv.config({
+    path: './env'
+})
+
+
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
+
+
+
+//SECOND APPROACH:
+// import express from "express";
+// const app = express();
+
+// ;( async () => {
+//     try {
+//       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//       app.on("error", (error) => {
+//         console.log("ERROR", error);
+//         throw error
+//       })
+
+//       app.listen(process.env.PORT, () => {
+//         console.log(`App is litening on port ${process.env.PORT}`)
+//       })
+//     } catch (error) {
+//         console.error("ERROR:", error)
+//         throw err
+
+//     }
+// })();
