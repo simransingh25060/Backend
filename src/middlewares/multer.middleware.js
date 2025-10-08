471456477210cab62b3dab30748c76aa4b8ce9f8
Simran Singh <simransingh25060@gmail.com>
2025-10-09 03:08:44 +0530

    Implement file upload

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,21 @@
+
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "./public/temp")
+    },
+    filename: function (req, file, cb) {
+      
+      cb(null, file.originalname)
+    }
+  })
+  
+export const upload = multer({ 
+    storage, 
+})
+
+
+
+
+//parses the incoming request and extracts the files
