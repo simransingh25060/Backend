3c722856066d02435b6a80b840a4206964bbc48f
Simran Singh <simransingh25060@gmail.com>
2025-10-26 18:04:36 +0530

    ADD: watchHistory endpoint and routes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -406,8 +406,64 @@ const getUserChannelProfile = asyncHandler(async(req, res) => {
 })
 
 
+const getWatchHistory = asyncHandler(async(req, res) => {
+    const user = await User.aggregation ([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "uploadedBy",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullName: 1,
+                                        username: 1,
+                                         avatar: 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields:{
+                            owner:{
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
 
-export { registerUser,
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            user[0].watchHistory,
+            "Watch history fetched successfully"
+        )
+    )
+})
+
+
+
+export { 
+    registerUser,
     loginUser,
     logoutUser,
     refreshAccessToken,
@@ -416,7 +472,8 @@ export { registerUser,
     updateAccountDetails,
     updateUserAvatar,
     updateUserCoverImage,
-    getUserChannelProfile} 
+    getUserChannelProfile,
+    getWatchHistory } 
 
 
 //accesstoken- short lived token
