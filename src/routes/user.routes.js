5dce6d0595643f0bfd351033b78ff8850bedf4d4
Simran Singh <simransingh25060@gmail.com>
2025-10-26 18:57:31 +0530

    ADD: models

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -9,7 +9,8 @@ import {
     updateUserAvatar,
     updateUserCoverImage,
     getUserChannelProfile,
-    getWatchHistory
+    getWatchHistory,
+    updateAccountDetails
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
